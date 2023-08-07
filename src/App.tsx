e5050a79aa0bb50f59012814c8e0770e897c59be
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-07 14:30:37 +0200

    registration and login pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { MantineProvider } from '@mantine/core'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
+
 
 export default function App() {
     return (
         <MantineProvider theme={{ colorScheme: 'dark' }} withGlobalStyles withNormalizeCSS>
-            <p>
-                Hello world
-            </p>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/register" element={ <RegisterPage /> } />
+                    <Route path="/login" element={ <LoginPage /> } />
+                </Routes>
+            </BrowserRouter>
         </MantineProvider>
     );
 }

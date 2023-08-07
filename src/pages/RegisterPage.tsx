47f10ecfc42087cbf241a7b091a6b5393813c52d
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-07 15:18:58 +0200

    form on register page

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,8 +1,22 @@
+import { Box, Button, Group, PasswordInput, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
+
 export default function RegisterPage()
 {
+    const form = useForm({
+        initialValues: { emailAddress: '', password: '' }
+    });
+
     return (
-        <p>
-            Registration Page
-        </p>
+        <Box maw={320} mx="auto">
+            <form onSubmit={form.onSubmit(values => console.log(values))}>
+                <TextInput label="Email Address" placeholder="Email Address" {...form.getInputProps('emailAddress')} />
+                <PasswordInput label="Password" placeholder="Password" {...form.getInputProps('password')} />
+
+                <Group position="right" mt="md">
+                    <Button type="submit">Register</Button>
+                </Group>
+            </form>
+        </Box>
     );
 }

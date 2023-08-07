8f67cd732514eeb04cdaca5d6800266f57cf1902
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-07 16:56:22 +0200

    prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
     'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',

8c176730be68721497a6068581845489b99b9935
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-08 21:25:50 +0200

    initial access token for debugging purposes

--- src/auth/provider.tsx
+++ src/auth/provider.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { AuthContext } from './context';
+import { createInitialAccessToken } from '@/settings';
 
 
 export function AuthProvider( { children }: { children: React.ReactNode; } ): JSX.Element
 {
-    const [ token, setToken ] = useState<string | undefined>( undefined );
+    const [ token, setToken ] = useState<string | undefined>( createInitialAccessToken() );
 
     const login = ( token: string ) => { setToken( token ); };
     const logout = () => { setToken( undefined ); };
---
+++ src/settings.ts
@@ -0,0 +1,9 @@
+export function createInitialAccessToken(): string | undefined
+{
+    // For production
+    // return undefined;
+
+    // TODO Needs to be removed!
+    // Seller
+    return `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwiZXhwIjoxNzIzMDI2NTkzLCJzY29wZXMiOlsiaXRlbXM6bGlzdC1vd24iLCJldmVudHM6bGlzdCIsIml0ZW1zOmFkZCJdfQ.tIR_7Y9CG9wCTQRYH-wWTsaeUokkMWcEzR3AkQCjeX4`;
+}

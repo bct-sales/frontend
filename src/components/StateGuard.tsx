e62886c8130199616a28670ec7a1ed136de3080f
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-10 21:22:17 +0200

    StateGuard

---
+++ src/components/StateGuard.tsx
@@ -0,0 +1,36 @@
+import { useLocation } from "react-router-dom";
+
+interface Props<T>
+{
+    predicate: (state: unknown) => state is T;
+
+    child: (value: T) => JSX.Element;
+
+    error?: () => JSX.Element;
+}
+
+
+export default function StateGuard<T>(props: Props<T>): JSX.Element
+{
+    const location = useLocation();
+    const state = location.state as unknown;
+
+    if ( props.predicate(state) )
+    {
+        return props.child(state);
+    }
+    else
+    {
+        const error = props.error ?? defaultError;
+
+        return error();
+    }
+}
+
+
+function defaultError()
+{
+    return (
+        <p>An error occurred</p>
+    );
+}

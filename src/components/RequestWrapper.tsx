a1c3cb883d71e02f6d18aede14ca1b42d2431b88
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-10 09:43:51 +0200

    RequestWrapper

---
+++ src/components/RequestWrapper.tsx
@@ -0,0 +1,58 @@
+import { RequestResult } from "@/rest/request";
+
+
+interface RequestWrapperProps<T, E>
+{
+    requestResult: RequestResult<T, E>;
+
+    loading?: () => JSX.Element;
+
+    failure?: (error: E) => JSX.Element;
+
+    success: (payload: T) => JSX.Element;
+}
+
+
+export default function RequestWrapper<T, E>(props: RequestWrapperProps<T, E>): JSX.Element
+{
+    const { requestResult } = props;
+
+    if ( requestResult.ready )
+    {
+        if ( requestResult.success )
+        {
+            return props.success(requestResult.payload);
+        }
+        else
+        {
+            const failure = props.failure ?? defaultFailure;
+
+            return failure(requestResult.error);
+        }
+    }
+    else
+    {
+        const loading = props.loading ?? defaultLoading;
+
+        return loading();
+    }
+
+
+    function defaultLoading(): JSX.Element
+    {
+        return (
+            <>
+                Loading!
+            </>
+        );
+    }
+
+    function defaultFailure(error: E): JSX.Element
+    {
+        return (
+            <>
+                Error: {error}
+            </>
+        )
+    }
+}

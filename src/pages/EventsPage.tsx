38b5772e8bf4e17f90ce39d312eaed397e25b345
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-10 12:29:25 +0200

    improved type safety of EventsPage

--- src/pages/EventsPage.tsx
+++ src/pages/EventsPage.tsx
@@ -1,4 +1,4 @@
-import { AuthenticatedUser } from "@/auth/types";
+import { AuthenticatedSeller } from "@/auth/types";
 import RequestWrapper from "@/components/RequestWrapper";
 import { listEvents } from "@/rest/events";
 import { SalesEvent } from "@/rest/models";
@@ -11,7 +11,7 @@ import { useNavigate } from "react-router-dom";
 
 interface EventsPageProps
 {
-    auth: AuthenticatedUser;
+    auth: AuthenticatedSeller;
 }
 
 
@@ -30,7 +30,7 @@ export default function EventsPage({ auth }: EventsPageProps): JSX.Element
 }
 
 
-function ActualEventsPage(props: { auth: AuthenticatedUser, events: SalesEvent[] }): JSX.Element
+function ActualEventsPage(props: { auth: AuthenticatedSeller, events: SalesEvent[] }): JSX.Element
 {
     const { events } = props;
 

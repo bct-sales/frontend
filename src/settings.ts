2ea8a4d28c0c2304b58a6cf661c9a15763d6163c
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-10 20:28:54 +0200

    new access token

--- src/settings.ts
+++ src/settings.ts
@@ -8,7 +8,7 @@ export function createInitialAuthentication(): { emailAddress: string, role: Rol
 
     // TODO Needs to be removed!
     // Seller
-    const accessToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwiZXhwIjoxNzIzMDI2NTkzLCJzY29wZXMiOlsiaXRlbXM6bGlzdC1vd24iLCJldmVudHM6bGlzdCIsIml0ZW1zOmFkZCJdfQ.tIR_7Y9CG9wCTQRYH-wWTsaeUokkMWcEzR3AkQCjeX4`;
+    const accessToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwiZXhwIjoxNzIzMjIyODE0LCJzY29wZXMiOlsiaXRlbXM6YWRkLW93biIsIml0ZW1zOmVkaXQtb3duIiwiaXRlbXM6bGlzdC1vd24iLCJldmVudHM6bGlzdCJdfQ.aoJWhO_2BsvrdzhsTDoO2ASosNavdw1j4Vw-uj_RS1A`;
     const emailAddress = `seller@bct.be`;
     const role = 'seller';
     return { emailAddress, role, accessToken };

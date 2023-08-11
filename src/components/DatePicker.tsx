17695750ee50a2dbac9f3bcdd9e9bc9422ac6723
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-11 17:05:12 +0200

    added label to DatePicker

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -7,12 +7,14 @@ interface Props
     date: BCTDate;
 
     onChange: (date: BCTDate) => void;
+
+    label?: string;
 }
 
 export default function DatePicker(props: Props): React.ReactNode
 {
     return (
-        <DateInput value={props.date.toDate()} onChange={onChange} />
+        <DateInput label={props.label} value={props.date.toDate()} onChange={onChange} />
     );
 
 

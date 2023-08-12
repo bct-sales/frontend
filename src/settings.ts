f2262caa378c14addc381ffa4ab5d4f758d1832d
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-12 16:21:03 +0200

    new auth settings

--- src/settings.ts
+++ src/settings.ts
@@ -15,7 +15,7 @@ export function createInitialAuthentication(): AuthenticationData | undefined
 
 function createSellerAuthentication(): AuthenticationData
 {
-    const accessToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwiZXhwIjoxNzIzMjIyODE0LCJzY29wZXMiOlsiaXRlbXM6YWRkLW93biIsIml0ZW1zOmVkaXQtb3duIiwiaXRlbXM6bGlzdC1vd24iLCJldmVudHM6bGlzdCJdfQ.aoJWhO_2BsvrdzhsTDoO2ASosNavdw1j4Vw-uj_RS1A`;
+    const accessToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwiZXhwIjoxNzIzMzg1OTU3LCJzY29wZXMiOlsiaXRlbXM6ZWRpdC1vd24iLCJpdGVtczpsaXN0LW93biIsImV2ZW50czpsaXN0IiwiaXRlbXM6YWRkLW93biJdfQ.RiIcpyJTEDCCmTkXLrFTUI38B9ofQCeYTgTt4W3AhQ8`;
     const emailAddress = `seller@bct.be`;
     const role = 'seller';
     const userId = 1;
@@ -26,7 +26,7 @@ function createSellerAuthentication(): AuthenticationData
 
 function createAdminAuthentication(): AuthenticationData
 {
-    const accessToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyIiwiZXhwIjoxNzIzMjgzNTYzLCJzY29wZXMiOlsiaXRlbXM6bGlzdC1hbGwiLCJldmVudHM6bGlzdCIsImFjY291bnRzOmxpc3QiLCJldmVudHM6YWRkIl19.CfG85HYtiU4OU7UFf-_K4dGuwv6tCPrH3nxJaOlLo80`;
+    const accessToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyIiwiZXhwIjoxNzIzMzg1OTg2LCJzY29wZXMiOlsiZXZlbnRzOmxpc3QiLCJpdGVtczpsaXN0LWFsbCIsImV2ZW50czphZGQiLCJldmVudHM6ZWRpdCIsImFjY291bnRzOmxpc3QiXX0.NYGc1lfH2pzLsc_NRRGwsxVb6AF2GxzGk93AtZYtAtg`;
     const emailAddress = `admin@bct.be`;
     const role = 'admin';
     const userId = 2;

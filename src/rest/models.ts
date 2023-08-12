ed7bb3169f5a229f98a73167c066fd027756a136
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-12 16:06:32 +0200

    changed representation of SalesEvent

--- src/rest/models.ts
+++ src/rest/models.ts
@@ -4,66 +4,111 @@ import { BCTTime } from "@/time";
 import { MoneyAmount } from "@/money-amount";
 
 
-export class SalesEvent
+
+export interface SalesEvent
 {
-    constructor(public readonly data: RawSalesEvent)
-    {
-        // NOP
-    }
-
-    public get salesEventId(): number
-    {
-        return this.data.sales_event_id;
-    }
-
-    public get date(): BCTDate
-    {
-        return BCTDate.parseIso(this.data.date);
-    }
-
-    public get startTime(): BCTTime
-    {
-        return BCTTime.parseIso(this.data.start_time);
-    }
-
-    public get endTime(): BCTTime
-    {
-        return BCTTime.parseIso(this.data.end_time);
-    }
-
-    public get location(): string
-    {
-        return this.data.location;
-    }
-
-    public get description(): string
-    {
-        return this.data.description;
-    }
-
-    public get id(): number
-    {
-        return this.data.sales_event_id;
-    }
-
-    public get raw(): RawSalesEvent
-    {
-        return this.data;
-    }
-
-    public updateDate(date: BCTDate): SalesEvent
-    {
-        return new SalesEvent({
-            ...this.data,
-            date: date.formatIso(),
-        });
-    }
-
-    public get links(): { edit: string }
-    {
-        return this.data.links;
+    id: number;
+    date: BCTDate;
+    startTime: BCTTime;
+    endTime: BCTTime;
+    location: string;
+    description: string;
+    links: {
+        edit: string,
     }
 }
+
+
+export function fromRawSalesEvent(raw: RawSalesEvent): SalesEvent
+{
+    return {
+        id: raw.sales_event_id,
+        date: BCTDate.parseIso(raw.date),
+        startTime: BCTTime.parseIso(raw.start_time),
+        endTime: BCTTime.parseIso(raw.end_time),
+        location: raw.location,
+        description: raw.description,
+        links: raw.links,
+    };
+}
+
+
+// export class SalesEvent
+// {
+//     constructor(public readonly data: RawSalesEvent)
+//     {
+//         // NOP
+//     }
+
+//     public get salesEventId(): number
+//     {
+//         return this.data.sales_event_id;
+//     }
+
+//     public get date(): BCTDate
+//     {
+//         return BCTDate.parseIso(this.data.date);
+//     }
+
+//     public get startTime(): BCTTime
+//     {
+//         return BCTTime.parseIso(this.data.start_time);
+//     }
+
+//     public get endTime(): BCTTime
+//     {
+//         return BCTTime.parseIso(this.data.end_time);
+//     }
+
+//     public get location(): string
+//     {
+//         return this.data.location;
+//     }
+
+//     public get description(): string
+//     {
+//         return this.data.description;
+//     }
+
+//     public get id(): number
+//     {
+//         return this.data.sales_event_id;
+//     }
+
+//     public get raw(): RawSalesEvent
+//     {
+//         return this.data;
+//     }
+
+//     public updateDate(date: BCTDate): SalesEvent
+//     {
+//         return new SalesEvent({
+//             ...this.data,
+//             date: date.formatIso(),
+//         });
+//     }
+
+//     public updateDescription(description: string): SalesEvent
+//     {
+//         return new SalesEvent({
+//             ...this.data,
+//             description,
+//         });
+//     }
+
+//     public updateLocation(location: string): SalesEvent
+//     {
+//         return new SalesEvent({
+//             ...this.data,
+//             location,
+//         });
+//     }
+
+//     public get links(): { edit: string }
+//     {
+//         return this.data.links;
+//     }
+// }
 
 
 export class Item

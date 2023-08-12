affac85454f1e32707d283ce9af4eadda7746bc6
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-12 16:11:01 +0200

    rest updateEvent

--- src/rest/events.ts
+++ src/rest/events.ts
@@ -1,7 +1,7 @@
 import { Result, failure, success } from '@/result';
 import axios from 'axios';
 import { extractDetailFromException } from './error-handling';
-import { SalesEvent } from './models';
+import { SalesEvent, fromRawSalesEvent } from './models';
 import { RawSalesEvents } from './raw-models';
 import { restUrl } from './url';
 
@@ -19,7 +19,7 @@ export async function listEvents(accessToken: string): Promise<Result<SalesEvent
         const response = await axios.get<unknown>( url, { headers } );
         const data = RawSalesEvents.parse(response.data);
 
-        return success(data.events.map(x => new SalesEvent(x)));
+        return success(data.events.map(raw => fromRawSalesEvent(raw)));
     }
     catch ( error: unknown )
     {
@@ -35,4 +35,23 @@ export async function listEvents(accessToken: string): Promise<Result<SalesEvent
             return failure<string>('Unknown error');
         }
     }
+}
+
+
+export async function updateEvent(accessToken: string, salesEvent: SalesEvent)
+{
+    const headers = {
+        Authorization: `Bearer ${accessToken}`
+    };
+
+    const url = salesEvent.links.edit;
+    const data = {
+        date: salesEvent.date.formatIso(),
+        start_time: salesEvent.startTime.formatIso(),
+        end_time: salesEvent.endTime.formatIso(),
+        location: salesEvent.location,
+        description: salesEvent.description,
+    };
+
+    await axios.put<unknown>( restUrl(url), data, { headers } );
 }

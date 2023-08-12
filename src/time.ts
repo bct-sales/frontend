6610fb7f85e52cf2ab540b9ff422a79d327404d1
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-12 15:47:37 +0200

    redesigned BCTTime so as to be serializable

--- src/time.ts
+++ src/time.ts
@@ -3,32 +3,39 @@ import moment from "moment";
 
 export class BCTTime
 {
-    public constructor(private moment: moment.Moment)
+    private constructor(private readonly isoRepresentation: string)
     {
         // NOP
     }
 
+    private static readonly isoFormattingString = 'HH:mm:ss';
+
     public static parse(str: string): BCTTime
     {
-        const result = moment(str, ['HH:mm']);
+        const result = moment(str, ['HH:mm']).format(BCTTime.isoFormattingString);
 
         return new BCTTime(result);
     }
 
     public static parseIso(str: string): BCTTime
     {
-        const result = moment(str, ['HH:mm:ss']);
+        const result = moment(str, BCTTime.isoFormattingString).format(BCTTime.isoFormattingString);
 
         return new BCTTime(result);
     }
 
+    public toMoment(): moment.Moment
+    {
+        return moment(this.isoRepresentation, BCTTime.isoFormattingString);
+    }
+
     public format(): string
     {
-        return this.moment.format('HH:mm');
+        return this.toMoment().format('HH:mm');
     }
 
     public formatIso(): string
     {
-        return this.moment.format('HH:mm:ss');
+        return this.isoRepresentation;
     }
 }

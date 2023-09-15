38e04cb166f8cb408a9354ff0ac19ae760fba5f2
Frederic Vogels <frederic.vogels+github@gmail.com>
2023-09-15 21:15:59 +0200

    GenerateLabelsPage

---
+++ src/components/IntegerInput.tsx
@@ -0,0 +1,35 @@
+import { TextInput } from "@mantine/core";
+
+
+interface Props
+{
+    value: number;
+
+    label: string;
+
+    onChange: (value: number) => void;
+}
+
+export function IntegerInput(props: Props): JSX.Element
+{
+    return (
+        <TextInput value={props.value} label={props.label} onChange={onChange} type="number" />
+    );
+
+
+    function onChange(event: React.ChangeEvent<HTMLInputElement>): void
+    {
+        const input = event.target.value;
+        const value = parseInt(input);
+
+        if ( Number.isInteger(value) )
+        {
+            console.log(value);
+            props.onChange(value);
+        }
+        else
+        {
+            props.onChange(0);
+        }
+    }
+}
--- src/pages/seller/GenerateLabelsPage.tsx
+++ src/pages/seller/GenerateLabelsPage.tsx
@@ -1,5 +1,8 @@
 import { AuthenticatedSellerStatus } from "@/auth/types";
+import { IntegerInput } from "@/components/IntegerInput";
 import PersistentStateGuard from "@/components/PersistentStateGuard";
+import { Button, Center, Group, Paper, Stack, Title } from "@mantine/core";
+import React from "react";
 import { z } from "zod";
 
 
@@ -33,10 +36,33 @@ export default function GenerateLabelsPage(props: { auth: AuthenticatedSellerSta
 function ActualGenerateLabelsPage(props: { auth: AuthenticatedSellerStatus, eventId: number }): JSX.Element
 {
     // const navigate = useNavigate();
+    const [pageWidth, setPageWidth] = React.useState<number>(210);
+    const [pageHeight, setPageHeight] = React.useState<number>(297);
+    const [columnCount, setColumnCount] = React.useState<number>(3);
+    const [rowCount, setRowCount] = React.useState<number>(8);
+    const [labelWidth, setLabelWidth] = React.useState<number>(80);
+    const [labelHeight, setLabelHeight] = React.useState<number>(50);
 
     return (
         <>
-            {props.eventId}
+            <Paper mx='auto' p="md" maw={1000}>
+            <Group position="apart">
+                    <Title>
+                        Generate Labels
+                    </Title>
+                </Group>
+                <Stack maw={400} mx='auto' my='xl'>
+                    <IntegerInput label="Page Width (mm)" onChange={setPageWidth} value={pageWidth} />
+                    <IntegerInput label="Page Height (mm)" onChange={setPageHeight} value={pageHeight} />
+                    <IntegerInput label="Column Count" onChange={setColumnCount} value={columnCount} />
+                    <IntegerInput label="Row Count" onChange={setRowCount} value={rowCount} />
+                    <IntegerInput label="Label Width" onChange={setLabelWidth} value={labelWidth} />
+                    <IntegerInput label="Label Height" onChange={setLabelHeight} value={labelHeight} />
+                </Stack>
+                <Center>
+                    <Button mx='auto'>Generate Labels</Button>
+                </Center>
+            </Paper>
         </>
     );
 }

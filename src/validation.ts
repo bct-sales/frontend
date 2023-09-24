96fc592b652d72e17f7875f4cff406dbaaa7faf1
Frederic Vogels <frederic.vogels+github@gmail.com>
2023-09-24 22:14:37 +0200

    removed email validator

--- src/validation.ts
+++ src/validation.ts
@@ -1,11 +1,3 @@
-import { validate } from 'email-validator';
-
-
-export function isValidEmailAddress(value: string): boolean
-{
-    return validate(value);
-}
-
 export function isValidPassword(value: string): boolean
 {
     return value.length >= 8;
